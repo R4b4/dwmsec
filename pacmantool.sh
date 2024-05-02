627228874eaefc9622ab06e21d50334b27e4498b
Whoami <83014786+whoamipwn@users.noreply.github.com>
2024-05-02 17:13:51 -0300

    Update pacmantool.sh

--- pacmantool.sh
+++ pacmantool.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo pacman -Suuyy  mpv mlocate papirus-icon-theme flameshot htop noto-fonts-emoji  xreader neofetch  qt5ct lxappearance  lm_sensors  dunst  alsa-tools alsa-utils net-tools  scrot qbittorrent tar numlockx   polkit-gnome   terminus-font ttf-jetbrains-mono-nerd  feh thunar git neovim  gvfs file-roller make nmap gdb ranger pavucontrol
+sudo pacman -Suuyy  mpv mlocate  ttf-font-awesome papirus-icon-theme flameshot htop noto-fonts-emoji  xreader neofetch  qt5ct lxappearance  lm_sensors  dunst  alsa-tools alsa-utils net-tools  scrot qbittorrent tar numlockx   polkit-gnome   terminus-font ttf-jetbrains-mono-nerd  feh thunar git neovim  gvfs file-roller make nmap gdb ranger pavucontrol
